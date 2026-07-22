1ac7b0d096c540ddd9a8a15467f03a8b4f5a7005
Nick Stebunov <mailbox@stebunoff.ru>
2026-07-22 23:22:30 +0300

    Lesson 2

---
+++ lesson_2/task2_2.py
@@ -0,0 +1,158 @@
+# Lesson 2
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+class LinkedList2:  
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+
+    # task 2.1 (find)
+    # Time complexity O(n)
+    # Space complexity O(1)
+    def find(self, val):
+        item = self.head
+
+        while item is not None:
+            if item.value == val:
+                return item
+            item = item.next
+
+        return None
+
+    # task 2.2 (find_all)
+    # Time complexity O(n)
+    # Space complexity O(n)
+    def find_all(self, val):
+        found = []
+        item = self.head
+        while item is not None:
+            if item.value == val:
+                found.append(item)
+            item = item.next
+        
+        return found
+
+    # task 2.3 (delete with false flag)
+    # Time complexity O(n)
+    # Space complexity O(1)
+
+    # task 2.4 (delete with true flag)
+    # Time complexity O(n)
+    # Space complexity O(1)
+    def delete(self, val, all=False):
+        item = self.head
+        while item is not None:
+            if item.value == val:
+                if item.prev is None:
+                    self.head = item.next
+                else:
+                    item.prev.next = item.next
+                
+                if item.next is None:
+                    self.tail = item.prev
+                else:
+                    item.next.prev = item.prev 
+
+                if not all:
+                    return
+            item = item.next
+
+    # task 2.7 (clean)
+    # Time complexity O(1)
+    # Space complexity O(1)
+    def clean(self):
+        self.head = None
+        self.tail = None
+
+    # task 2.8 (len)
+    # Time complexity O(n)
+    # Space complexity O(1)
+    def len(self):
+        length = 0
+        item = self.head
+        while item is not None:
+            length += 1
+            item = item.next
+        return length
+
+    # task 2.5 (insert)
+    # Time complexity O(1)
+    # Space complexity O(1)
+    def insert(self, afterNode, newNode):
+        if afterNode is None and self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        elif afterNode is None:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        else:
+            newNode.next = afterNode.next
+            if afterNode.next is not None:
+                afterNode.next.prev = newNode
+            else:
+                self.tail = newNode
+            afterNode.next = newNode
+            newNode.prev = afterNode
+
+    # task 2.6 (add_in_head)
+    # Time complexity O(1)
+    # Space complexity O(1)
+    def add_in_head(self, newNode):
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+
+    # task 2.10 (revert)
+    # Time complexity O(n)
+    # Space complexity O(1)
+    def revert(self):
+        if self.head is None:
+            return
+        
+        item = self.head
+        newTail = self.head
+        newHead = self.tail
+        while item is not None:
+            copy = item
+            item.next = copy.prev
+            item.prev = copy.next
+            item = copy.next
+        self.head, self.tail = newHead, newTail
+
+
+    # task 2.11 (cycles check)
+    # Time complexity O(n)
+    # Space complexity O(n)
+    def cycle_presence(self):
+        visited = set()
+        item = self.head
+
+        while item is not None:
+            if item in visited:
+                return True
+
+            visited.add(item)
+            item = item.next
+
+        return False
+
+
---
+++ lesson_2/task2_3.py
@@ -0,0 +1,290 @@
+# Lesson 2
+import unittest
+
+class TestLinkedList2Find(unittest.TestCase):
+    def test_find_empty_list(self):
+        linked_list = LinkedList2()
+        self.assertEqual(linked_list.find(1), None)
+
+    def test_find_nonempty_list_one_match(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(2)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertIs(linked_list.find(2), nodes[1])
+
+    def test_find_nonempty_list_no_matches(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(2)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertIsNone(linked_list.find(3))
+
+class TestLinkedList2FindAll(unittest.TestCase):
+    def test_find_all_empty_list(self):
+        linked_list = LinkedList2()
+        self.assertEqual(linked_list.find_all(1), [])
+
+    def test_find_all_nonempty_list_one_match(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(2)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertEqual(linked_list.find_all(2), [nodes[1], nodes[2]])
+
+    def test_find_all_nonempty_list_no_matches(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(2)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertEqual(linked_list.find_all(3), [])
+
+class TestLinkedList2Delete(unittest.TestCase):
+    def test_delete_from_beginning(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.delete(1)
+        self.assertEqual(linked_list.len(), 2)
+        self.assertIs(linked_list.head, nodes[1])
+        self.assertIs(linked_list.tail, nodes[2])
+        self.assertIsNone(linked_list.head.prev)
+        self.assertIsNone(linked_list.tail.next)
+        self.assertIs(linked_list.head.next, linked_list.tail)
+        self.assertIs(linked_list.tail.prev, linked_list.head)
+
+    def test_delete_from_middle(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.delete(2)
+        self.assertEqual(linked_list.len(), 2)
+        self.assertIs(linked_list.head, nodes[0])
+        self.assertIs(linked_list.tail, nodes[2])
+        self.assertIs(linked_list.tail.prev, linked_list.head)
+        self.assertIs(linked_list.head.next, linked_list.tail)
+        self.assertIsNone(linked_list.head.prev)
+        self.assertIsNone(linked_list.tail.next)
+
+    def test_delete_from_end(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.delete(3)
+        self.assertEqual(linked_list.len(), 2)
+        self.assertIs(linked_list.head, nodes[0])
+        self.assertIs(linked_list.tail, nodes[1])
+        self.assertIs(linked_list.tail.prev, linked_list.head)
+        self.assertIs(linked_list.head.next, linked_list.tail)
+        self.assertIsNone(linked_list.head.prev)
+        self.assertIsNone(linked_list.tail.next)
+
+    def test_delete_with_true_flag(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(2)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.delete(2, all=True)
+        self.assertEqual(linked_list.len(), 1)
+        self.assertIs(linked_list.head, nodes[0])
+        self.assertIs(linked_list.tail, nodes[0])
+        self.assertIsNone(linked_list.head.prev)
+        self.assertIsNone(linked_list.tail.next)
+
+class TestLinkedList2Insert(unittest.TestCase):
+    def test_insert_in_empty_list(self):
+        linked_list = LinkedList2()
+        node = Node(1)
+        linked_list.insert(None, node)
+
+        self.assertEqual(linked_list.len(), 1)
+        self.assertIs(linked_list.head, node)
+        self.assertIs(linked_list.tail, node)
+        self.assertIsNone(linked_list.head.prev)
+        self.assertIsNone(linked_list.tail.next)
+
+    def test_insert_in_tail(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        node = Node(4)
+        linked_list.insert(None, node)
+        self.assertEqual(linked_list.len(), 4)
+        self.assertIs(linked_list.head, nodes[0])
+        self.assertIs(linked_list.tail, node)
+        self.assertIsNone(linked_list.tail.next)
+        self.assertIs(linked_list.tail.prev, nodes[2])
+        self.assertIs(nodes[2].next, node)
+
+    def test_insert_after_node(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        node = Node(4)
+        linked_list.insert(nodes[1], node)
+        self.assertEqual(linked_list.len(), 4)
+        self.assertIs(linked_list.head, nodes[0])
+        self.assertIs(linked_list.tail, nodes[2])
+        self.assertIs(nodes[1].next, node)
+        self.assertIs(node.prev, nodes[1])
+        self.assertIs(nodes[2].prev, node)
+        self.assertIs(node.next, nodes[2])
+
+class TestLinkedList2AddInHead(unittest.TestCase):
+    def test_add_in_head_in_empty_list(self):
+        linked_list = LinkedList2()
+        node = Node(1)
+        linked_list.add_in_head(node)
+        self.assertEqual(linked_list.len(), 1)
+        self.assertIs(linked_list.head, node)
+        self.assertIs(linked_list.tail, node)
+        self.assertIsNone(node.prev)
+        self.assertIsNone(node.next)
+
+    def test_add_in_head_in_nonempty_list(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        node = Node(4)
+        linked_list.add_in_head(node)
+        self.assertEqual(linked_list.len(), 4)
+        self.assertIs(linked_list.head, node)
+        self.assertIs(nodes[0].prev, node)
+        self.assertIs(node.next, nodes[0])
+        self.assertIsNone(node.prev)
+        self.assertIs(linked_list.tail, nodes[2])
+        self.assertIsNone(linked_list.tail.next)
+
+class TestLinkedList2Clean(unittest.TestCase):
+    def test_clean(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.clean()
+        self.assertEqual(linked_list.len(), 0)
+        self.assertIsNone(linked_list.head)
+        self.assertIsNone(linked_list.tail)
+
+class TestLinkedList2Len(unittest.TestCase):
+    def test_len_empty_list(self):
+        linked_list = LinkedList2()
+        self.assertEqual(linked_list.len(), 0)
+
+    def test_len_nonempty_list(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertEqual(linked_list.len(), 3)
+
+class TestLinkedList2Revert(unittest.TestCase):
+    def test_revert_empty_list(self):
+        linked_list = LinkedList2()
+        linked_list.revert()
+        self.assertIsNone(linked_list.head)
+        self.assertIsNone(linked_list.tail)
+        self.assertEqual(linked_list.len(), 0)
+
+    def test_revert_one_element_list(self):
+       linked_list = LinkedList2() 
+       node = Node(1)
+       linked_list.add_in_tail(node)
+       linked_list.revert()
+       self.assertIs(self.head, node)
+       self.assertIs(self.tail, node)
+       self.assertEqual(linked_list.len(), 1)
+       self.assertIsNone(node.prev)
+       self.assertIsNone(node.next)
+
+    def test_revert_list(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        linked_list.revert()
+        self.assertIs(linked_list.head, nodes[2])
+        self.assertIs(linked_list.tail, nodes[0])
+        self.assertIs(nodes[0].prev, nodes[1])
+        self.assertIsNone(nodes[0].next)
+        self.assertIs(nodes[1].prev, nodes[2])
+        self.assertIs(nodes[1].next, nodes[0])
+        self.assertIs(nodes[2].next, nodes[1])
+        self.assertIsNone(nodes[2].prev)
+
+class TestLinkedList2CyclePresence(unittest.TestCase):
+    def test_cycle_presence_empty_list(self):
+        linked_list = LinkedList2()
+        self.assertEqual(linked_list.cycle_presence(), False)
+
+    def test_cycle_presence_one_element_list(self):
+        linked_list = LinkedList2()
+        linked_list.add_in_tail(Node(1))
+        self.assertEqual(linked_list.cycle_presence(), False)
+
+    def test_cycle_presence_next_item_cycle(self):
+        linked_list = LinkedList2()
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        linked_list.add_in_tail(node1)
+        linked_list.add_in_tail(node2)
+        linked_list.add_in_tail(node3)
+        node2.next = node1
+        self.assertEqual(linked_list.cycle_presence(), True)
+
+    def test_cycle_presence_item_long_cycle(self):
+        linked_list = LinkedList2()
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        linked_list.add_in_tail(node1)
+        linked_list.add_in_tail(node2)
+        linked_list.add_in_tail(node3)
+        node3.next = node1
+        self.assertEqual(linked_list.cycle_presence(), True)
+
+    def test_cycle_presence_nonempty_list(self):
+        linked_list = LinkedList2()
+        nodes = [Node(1), Node(2), Node(3)]
+
+        for node in nodes:
+            linked_list.add_in_tail(node)
+
+        self.assertEqual(linked_list.cycle_presence(), False)
+
+
